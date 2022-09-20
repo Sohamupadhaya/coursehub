b5e331f59eff4d23ed364de2319bbfea66e1856f
Sohamupadhaya <54521881+Sohamupadhaya@users.noreply.github.com>
2022-09-21 01:38:00 +0530

    enroll completed validation and authentication is remaning

--- controller/enroll.js
+++ controller/enroll.js
@@ -23,3 +23,65 @@ exports.createEnroll = async (req,res) =>{
         res.send("error");
     }
 }
+
+exports.getEnrollById = async (req, res) =>{
+    try{
+
+        const getEnroll = await Enroll.findById(req.params.enrollId);
+        if(!getEnroll){
+            res.status(404).json({"message":"No Id was Found"})
+        }
+        res.json(getEnroll)
+    }
+    catch (err) {
+        console.log(err);
+        res.send("Error")
+    }
+}
+
+exports.getAllEnroll = async (req, res) =>{
+    try{
+        const allEnrolls = await Enroll.find();
+        if (allEnrolls.length <= 0){
+            res.json({"message":"No Data In  DataBase"});
+        }
+        res.json(allEnrolls)
+    }
+    catch (err) {
+        console.log(err);
+        res.send("Error")
+    }
+}
+
+exports.editEnroll = async (req, res) =>{
+    try{
+        const getEnroll = await Enroll.findById({_id:req.params.enrollId});
+        if(!getEnroll){
+            return res.status(404).json({"message":"No Id was Found"})
+        }
+        allUpdateEnroll = {
+            course:req.body.course,
+            userId:req.body.userId,
+            progress:[]
+        }
+        for (let[index,value] of req.body.progress.entries()){
+            progressData={}
+            progressData.slideNo = value.slideNo;
+            progressData.enteredCode = value.enteredCode;
+            allUpdateEnroll.progress.push(progressData)
+        }
+        console.log(allUpdateEnroll);
+        const updateEnroll = await Enroll.findByIdAndUpdate({_id:req.params.enrollId},
+            {$set:allUpdateEnroll},
+            { new: true, upsert: true }
+            );
+        if(!updateEnroll){
+            return res.status(400).json({"message":"Problem in update"})
+        }
+        res.json(updateEnroll)
+    }
+    catch (err) {
+        console.log(err);
+        res.send("Error")
+    }
+}
--- routes/enroll.js
+++ routes/enroll.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createEnroll } = require("../controller/enroll");
+const { createEnroll,getEnrollById,getAllEnroll,editEnroll} = require("../controller/enroll");
 
 router.post("/create/", createEnroll);
+router.get("/dislpay/:enrollId/", getEnrollById);
+router.get("/enrolls/", getAllEnroll);
+router.put("/edit/:enrollId/",editEnroll);
 
 module.exports = router;
